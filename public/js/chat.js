994673a7408036fb0fde93fc5cab8923ad7d61bb
xeusteerapat <xeus085@gmail.com>
2021-02-26 02:01:30 +0700

    [EDIT] :sparkles: setup client input

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io();
+const socket = io();
+
+socket.on('message', message => {
+  console.log('Message from server: ', message);
+});
+
+document.querySelector('#message').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+  socket.emit('message', message);
+});
