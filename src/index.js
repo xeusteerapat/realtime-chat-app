1c9d106e7d7d76b90205715013a4b1040fd3c946
xeusteerapat <xeus085@gmail.com>
2021-02-26 00:21:59 +0700

    [INIT] :sparkles: setup socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+io.on('connection', () => {
+  console.log('New socket connected...');
+});
+
+server.listen(3000, () => {
   console.log(`Server on port 3000`);
 });
