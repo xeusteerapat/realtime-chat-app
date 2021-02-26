67fb2f11af534632493de6e5d3ead116e84c7523
xeusteerapat <xeus085@gmail.com>
2021-02-26 14:42:10 +0700

    [EDIT] :sparkles: add time

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,31 @@ const messageForm = document.getElementById('message-form');
 const messageFormInput = messageForm.querySelector('input');
 const messageFormButton = messageForm.querySelector('button');
 const sendLocationButton = document.getElementById('send-location');
+const messages = document.getElementById('messages');
+
+// Templates
+const messageTemplate = document.getElementById('message-template').innerHTML;
+const locationMessageTemplate = document.getElementById(
+  'location-message-template'
+).innerHTML;
 
 socket.on('message', message => {
   console.log(message);
+
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm: a'),
+  });
+
+  messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', url => {
+  const html = Mustache.render(locationMessageTemplate, {
+    url,
+  });
+
+  messages.insertAdjacentHTML('beforeend', html);
 });
 
 messageForm.addEventListener('submit', e => {
