39d3c4914901e784306de8f96f95513af34a42c4
xeusteerapat <xeus085@gmail.com>
2021-02-27 13:52:49 +0700

    [EDIT] :wrench: update port

--- src/index.js
+++ src/index.js
@@ -92,6 +92,8 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
   console.log(`Server on port 3000`);
 });
